3823bdeeb2fe151b84cf9e773f04b9316e148ac0
emd <emd@mit.edu>
2015-10-06 16:29:19 -0700

    Tests for initialization exceptions.

--- tests/fir_tests.py
+++ tests/fir_tests.py
@@ -1,8 +1,15 @@
 from nose import tools
 import numpy as np
-from 
+from filters.fir import NER
 
 
-def test_NER__init__():
-    try:
-        filt = NER(
+@tools.raises(ValueError)
+def test_NER__init__ripple():
+    # This ripple size is too small
+    NER(0.1, [2., 3.], 1., 5.)
+
+
+@tools.raises(ValueError)
+def test_NER__init__trans():
+    # This transition size is too small for the given passband
+    NER(0.01, [2.25, 2.75], 1., 5., pass_zero=False)
